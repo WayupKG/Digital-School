3cf6999643b142726e46b501cfbe411fa0117aa0
Nursultan <abyleevnursultan14@gmail.com>
2022-03-26 20:11:58 +0600

    Add admin School

--- User/admin.py
+++ User/admin.py
@@ -11,5 +11,5 @@ class UserAdmin(admin.ModelAdmin):
 @admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
     model = Student
-    list_display = ('get_full_name', 'account', 'gender', 'school', 'edu_grade', 'created', 'updated')
+    list_display = ('get_full_name', 'account', 'gender', 'school', 'edu_grade', 'created_at', 'updated_at')
     list_filter = ('school', 'edu_grade', 'gender')
